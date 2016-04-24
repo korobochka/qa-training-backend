2c729ef550bbec111079d9cd8b404f9a6f419212
tburlakova <matroskinatana@gmail.com>
2016-04-24 19:30:19 +0300

    add cross-origin support

--- src/main/java/com/griddynamics/Main.java
+++ src/main/java/com/griddynamics/Main.java
@@ -2,9 +2,13 @@ package com.griddynamics;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * Created by korobochka on 4/6/16.
@@ -18,7 +22,21 @@ public class Main {
 
     @Configuration
     static public class AppConfiguration extends RepositoryRestMvcConfiguration {
+        @Bean
+        public CorsFilter corsFilter() {
 
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowCredentials(true); // you USUALLY want this
+            config.addAllowedOrigin("*");
+            config.addAllowedHeader("*");
+            config.addAllowedMethod("GET");
+            config.addAllowedMethod("POST");
+            config.addAllowedMethod("PUT");
+            config.addAllowedMethod("DELETE");
+            source.registerCorsConfiguration("/**", config);
+            return new CorsFilter(source);
+        }
         @Override
         protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
             config.exposeIdsFor(Dog.class);
